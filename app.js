11cebafbe14de77135a9425b7f13cd52df732867
Shamin Ahsan <shamin.ahsan.01@gmail.com>
2026-07-21 15:15:46 -0400

    commit: added poll route

--- app.js
+++ app.js
@@ -1,7 +1,27 @@
+const express = require('express')
 const {db, Poll, Option, Vote} = require("./database_scripts/index");
+
+const app = express()
+const PORT = 3000
+
+app.use(express.json());
+
+app.get('/', async (req, res)=>{
+    try{
+        const data = await Poll.findAll()
+        res.json(data);
+    }catch(err){
+        console.error('Error fetching polls:', err);
+        res.status(500).json({error: 'Failed to fetch polls'})
+    } 
+})
+
 
 db.authenticate()
     .then(() => console.log('Connection established.'))
     .catch(err => console.error('Unable to connect:', err));
 
-db.sync({force: true});
+db.sync();
+app.listen(PORT, () => {
+    console.log(`Server actively running at http://localhost:${PORT}`);
+});
