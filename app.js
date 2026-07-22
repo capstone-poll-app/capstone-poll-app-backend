e12ff1b9ec268db43a1c4eb06afb808adff43071
Shamin Ahsan <shamin.ahsan.01@gmail.com>
2026-07-22 15:34:03 -0400

    changes to the db sync

--- app.js
+++ app.js
@@ -1,26 +1,23 @@
-const express = require('express')
-const pollRouter = require('./routes/poll')
-const {db, Poll, Option, Vote} = require("./database_scripts/index");
-const cors = require('cors')
+const express = require("express");
+const pollRouter = require("./routes/poll");
+const { db, Poll, Option, Vote } = require("./database_scripts/index");
+const cors = require("cors");
 
-
-const app = express()
-const PORT = 3000
+const app = express();
+const PORT = 3000;
 
 //middle-ware
 app.use(express.json());
-app.use(cors())
+app.use(cors());
 
-app.use('/', pollRouter)
+app.use("/", pollRouter);
 
+// db.authenticate()
+//   .then(() => console.log("Connection established."))
+//   .catch((err) => console.error("Unable to connect:", err));
 
-
-
-db.authenticate()
-    .then(() => console.log('Connection established.'))
-    .catch(err => console.error('Unable to connect:', err));
-
-db.sync();
-app.listen(PORT, () => {
+db.sync().then(() => {
+  app.listen(PORT, () => {
     console.log(`Server actively running at http://localhost:${PORT}`);
+  });
 });
